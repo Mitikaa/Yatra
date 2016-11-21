16796ca6f4ba3df109a84ac4154b83f19bd6e3a9
Mitikaa <sama.mitikaa@gmail.com>
2016-11-20 18:19:34 -0700

    Trip Dao updated

--- Yatra/src/yatra/controller/CommonController.java
+++ Yatra/src/yatra/controller/CommonController.java
@@ -14,16 +14,51 @@ import yatra.dao.TripDao;
  *
  */
 public class CommonController {
-	public static String TRIP_RDF = "/Users/Mitikaa/Desktop/Fall 2016/Semantic Web/Yatra/Yatra/Files/LocationRDF.rdf";
+	public static String TRIP_RDF = "/Users/Mitikaa/git/Yatra/Yatra/Files/LocationRDF.rdf";
 	static String defaultNameSpace = "http://www.semanticweb.org/ontologies/2016/12/Yatra#";
 	Model _trips = null;
 	static TripDao tripDao = new TripDao();
 	
 	public static void main(String[] args){
 		CommonController trip = new CommonController();
+		//for sparql
+		String source = "Los Angeles; California";
+		String destination = "Chicago; Illinois";
+		
 		trip.populateTrips();
-		System.out.println("Filters:");
-		tripDao.getFilters(trip._trips);
+		//System.out.println("Filters:");
+		//tripDao.getFilters(trip._trips);
+		
+		
+		System.out.println("Terminals:");
+		String resultTerminal = tripDao.getTerminalURIs(trip._trips, source, destination);
+		
+		//store source and destination URIs
+		String[] resultURIs = resultTerminal.split("\n");
+		String sourceURI = resultURIs[0];
+		String destinationURI = resultURIs[1];
+		System.out.println(sourceURI+"\n"+destinationURI);
+		
+		System.out.println("Trips:");
+		String resultTrip = tripDao.getTripURI(trip._trips, sourceURI, destinationURI);
+		
+		//store trip URIs
+		String[] tripURIs = resultTrip.split("\n");
+		String tripURI = tripURIs[0];
+		System.out.println(tripURI);
+		
+		System.out.println("Stations:");
+		String resultStationList = tripDao.getStationURIs(trip._trips, tripURI);
+		
+		//store station URis
+		String[] stationURIs = resultStationList.split("\n");
+		String filter = "politics";
+		for (int i=0; i<stationURIs.length; i++){			
+			String resultStationDetails = tripDao.getStationDetails(trip._trips, stationURIs[i], filter);
+			if(resultStationDetails!=null){
+				System.out.println(resultStationDetails);
+			}
+		}
 	}
 	
 	private void populateTrips(){
